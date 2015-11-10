2f824ba24c9d5275ba9f74ee7429111bc826b335
Killian Koenig <killiankoenig@gmail.com>
2015-11-09 23:01:18 -0800

    Switch to VS 2012 by default

--- make.cmd
+++ make.cmd
@@ -2,11 +2,11 @@ if not exist Build32 mkdir Build32
 if not exist Build64 mkdir Build64
 
 cd Build32
-cmake -G "Visual Studio 14 2015" ..
+cmake -G "Visual Studio 11 2012" ..
 cd ..
 
 cd Build64
-cmake -G "Visual Studio 14 2015 Win64" ..
+cmake -G "Visual Studio 11 2012 Win64" ..
 cd ..
 
 cmake --build Build32 --config Release
