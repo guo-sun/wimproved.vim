2fabc8b7b66dc8931ee74dfc78e38f1a64fc0323
kkoenig <killiankoenig@gmail.com>
2015-11-13 18:35:18 -0800

    Replace usage of Expand-Archive

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,10 +1,15 @@
+function unzip($path) {
+    $shell = new-object -com shell.application
+    $shell.namespace('.').copyhere($shell.namespace($path + ".zip").items()) 
+}
+
 (npm install) | Out-Null
 
 $path = $env:PATH
 
 if (-not (Test-Path 'vim74-kaoriya-win32' -PathType container)) {
     curl http://files.kaoriya.net/vim/vim74-kaoriya-win32.zip -OutFile vim74-kaoriya-win32.zip
-    Expand-Archive vim74-kaoriya-win32.zip
+    unzip  'vim74-kaoriya-win32'
 }
 
 Write-Output 'Testing wimproved32.dll'
@@ -13,7 +18,7 @@ $env:PATH = $path + ";$pwd\vim74-kaoriya-win32"
 
 if (-not (Test-Path 'vim74-kaoriya-win64' -PathType container)) {
     curl http://files.kaoriya.net/vim/vim74-kaoriya-win64.zip -OutFile vim74-kaoriya-win64.zip
-    Expand-Archive vim74-kaoriya-win64.zip
+    unzip  'vim74-kaoriya-win64'
 }
 
 Write-Output 'Testing wimproved64.dll'
